ee5a13dc41e254d0e1c1b15f87d20e157e847dc2
aayaami <vostroknutov.alexander@gmail.com>
2019-09-13 14:33:17 +0300

    Added layout, navbar and index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="row d-flex justify-content-center">
+    <div class="card bg-light m-5 p-4">
+      <h1 class="card-title text-center">Welcome To Que</h1>
+      <p class="card-text">In this application you can ask questions and answer questions from others.</p>
+    </div>
+  </div>
+@endsection
