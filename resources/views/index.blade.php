c4ec0fa27cd712c385b8be220eeb257e899b3f70
aayaami <vostroknutov.alexander@gmail.com>
2019-09-23 14:03:51 +0300

    Register\login buttons are now invisible for authtenticated users

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,9 @@
     <div class="jumbotron text-center card bg-light m-5 p-4">
       <h1 class="card-title">Welcome To Que</h1>
       <p class="card-text">In this application you can ask questions and answer questions from others.</p>
-      <p><a class="btn btn-primary btn-lg" href="/login">Login</a> <a class="btn btn-success btn-lg" href="/register">Register</a></p>
+      @guest
+        <p><a class="btn btn-primary btn-lg" href="/login">Login</a> <a class="btn btn-success btn-lg" href="/register">Register</a></p>
+      @endguest
     </div>
   </div>
 @endsection
